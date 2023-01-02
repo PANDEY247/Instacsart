c12fce3b1531a3165ceb7d3866955514742332df
sourabh <sp8046904@gmail.com>
2023-01-02 12:50:15 +0530

    first commit

---
+++ src/constants/growth.js
@@ -0,0 +1,18 @@
+export const growth = [
+  {
+    field: "500 million products",
+    description: "available to shop across the catalog",
+  },
+  {
+    field: "1000+ stores",
+    description: "from local grocers to chain stores",
+  },
+  {
+    field: "1000+ cities",
+    description: "served across the U.S. & Canada",
+  },
+  {
+    field: "Millions of orders",
+    description: "delivered or picked up yearly",
+  },
+];
---
+++ src/constants/programs_shopper.js
@@ -0,0 +1,44 @@
+export const programs_shopper = {
+  logo: "https://d2guulkeunn7d8.cloudfront.net/assets/footer/shopper-footer-v3-7e5a506c6d7f915192f8db4a5aa1b9a035f2612eb609988031e35d1573e66c0e.svg",
+  slogan: "Become a Shopper",
+  arr: [
+    {
+      title: "Make money with us",
+      body: [
+        "Shopper Opportunities",
+        "Become a Shopper",
+        "In-Store Employee For Instacart",
+        "Earnings"
+      ],
+    },
+    {
+      title: "Top cities",
+      body: [
+        "Delhi",
+        "GuruGram",
+        "Noida",
+        "Ghaziabad",
+        "Najafgarh"
+      ],
+    },
+    {
+      title: "More cities",
+      body: [
+        "jaipur",
+        "Udaipur",
+        "Mumbai",
+        "Pune",
+        "Bangalore",
+        
+      ],
+    },
+    {
+      title: "Shopper help",
+      body: [
+        "Help",
+        "Contact",
+        "Safety"
+      ],
+    },
+  ],
+};
